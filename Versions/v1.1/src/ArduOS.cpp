54fdb3462714435d768670a252b47504d340e24b
Ebubekir Dirican <67467774+EbkDrcn@users.noreply.github.com>
2020-11-20 13:42:45 +0300

    v1.1 güncellemesi
    Uzun ve kısa tıklama arasındaki fark yapıldı. Artık 0.5 saniye ve daha uzun tıklamalar uzun tıklama sayılacak. Eğer pin seçip uzun tıklarsanız sonsuza kadar blink yapacak. Kısa tıklarsanız 10 kere blink yapıp başa dönecek.

---
+++ Versions/v1.1/src/ArduOS.cpp
@@ -0,0 +1,104 @@
+// v1.1 Yenilikler : Uzun basarak sonsuz döngüye kısa basarak 10 kere blink
+
+/* ArduOS v1.1 beta by Ebubekir Dirican
+ * 
+ * ArduOS Arduino üzerinde çalışabilir olarak tasarlanmış bir işletim sistemidir.
+ * Şuanlık betadadır 
+ * Şuanlık yapabildikleri : 
+ * Seçtiğiniz bir pindeki LED'e "Blink" yaptırma 
+ * 
+ * Gerekenler : 
+ * 1 tane 16x2 LCD 
+ * 3 tane push buton 
+ * 1 tane LED
+ */
+ 
+const int but1 = 6, but2 = 7, but3 = 8; //Butonlar için pin 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;  //LCD nin pinleri
+int pin = 13; // LED pini için değişken
+int blinkpin;
+
+#include <LiquidCrystal.h>
+#include <Arduino.h>
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);
+  pinMode(but1, INPUT);
+  pinMode(but2, INPUT);
+  pinMode(but3, INPUT);
+  Serial.begin(9600);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Secilen Pin:");
+  lcd.setCursor(0,1);
+  lcd.print(pin);
+}
+
+void loop() {
+  lcd.setCursor(0,0);
+  lcd.print("Secilen Pin:");
+  lcd.setCursor(0,1);
+
+  if(digitalRead(but1) == 1){
+    delay(100);
+    pin = pin - 1;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Secilen Pin:");
+    lcd.setCursor(0,1);
+    lcd.print(pin);
+    
+  }
+  else if(digitalRead(but3)== 1)
+  {
+    delay(100);
+    pin = pin + 1;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Secilen Pin:");
+    lcd.setCursor(0,1);
+    lcd.print(pin);
+  }
+  else if (digitalRead(but2) == 1)
+  {
+    delay(500);
+   if (digitalRead(but2) == 1)
+   {
+     while (1)
+     {
+       digitalWrite(pin, 1);
+       delay(1000);
+       digitalWrite(pin, 0);
+       delay(1000);
+       lcd.clear();
+       lcd.setCursor(0,0);
+       lcd.print("Secilen Pin:");
+       lcd.setCursor(0,1);
+       lcd.print(pin);
+       Serial.print(pin);
+       Serial.println(" üzerinden blink işlemi başladı. Kodu kullandığınız için teşekkür ederiz. ArduOS by Ebubekir Dirican");
+     }
+   }
+   else
+   {
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print("Secilen Pin:");
+   lcd.setCursor(0,1);
+   lcd.print(pin);
+   Serial.print(pin);
+   Serial.println(" üzerinden blink işlemi başladı. Kodu kullandığınız için teşekkür ederiz. ArduOS by Ebubekir Dirican");
+   for (int i = 0; i < 10; i++)
+   {
+     pinMode(pin, OUTPUT);
+     digitalWrite(pin, 1);
+     delay(1000);
+     digitalWrite(pin, 0);
+     delay(1000); 
+     lcd.clear(); 
+   }
+  }
+ } 
+}
